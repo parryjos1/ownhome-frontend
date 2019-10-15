0139f84c02315fd9c9c45b08045bc81b3ff73666
Josh Parry <josh.parry97@gmail.com>
2019-10-15 19:57:54 +1100

    user signs up and is posted to backend

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,14 +1,16 @@
+
 import React from 'react';
-// import {HashRouter as Router, Route} from 'react-router-dom';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import {HashRouter as Router, Route} from 'react-router-dom';
+// import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 
 import Home from '../components/homepage';
 import Jobs from '../components/services/allJobs';
 import PostJob from '../components/services/PostJob/PostJob'
 import Job from '../components/services/job'
 import ServicesHome from '../components/services/homepage/ServicesHome';
+import Signup from '../components/services/Signup/signup'
 
-import Parent from '../components/services/PassData/Parent';
+// import Parent from '../components/services/PassData/Parent';
 
 const Routes = (
 
@@ -19,8 +21,9 @@ const Routes = (
     <Route exact path="/services/postjob" component={PostJob} />
     <Route exact path="/services/jobs/:id" component={Job} />
     <Route exact path="/services" component={ServicesHome} />
+    <Route exact path="/signup" component={Signup} />
 
-    <Route exact path="/services/parent" component={Parent} />
+    {/* <Route exact path="/services/parent" component={Parent} /> */}
 
   </Router>
 )
